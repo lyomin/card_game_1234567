de43f5b4c7a569a3e4341a9e9ef498da84f03f2b
Justin Byckopvic <justinbyckovic@gmail.com>
2026-07-08 16:41:23 +0300

    Adding backend

---
+++ scores/src/test/java/org/foo/scores/ScoresApplicationTests.java
@@ -0,0 +1,13 @@
+package org.foo.scores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScoresApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
