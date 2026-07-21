179c55ba824cd3bd8ce98e1bd5a7fe6a410359d1
Justin Byckopvic <justinbyckovic@gmail.com>
2026-07-21 14:31:05 +0300

    Building documentation

--- scores/src/main/java/org/foo/scores/model/Score.java
+++ scores/src/main/java/org/foo/scores/model/Score.java
@@ -1,8 +1,17 @@
 package org.foo.scores.model;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import org.jspecify.annotations.Nullable;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Table;
 
+@Schema(description = "Game mode results")
 @Table(name="SCORE_BOARD")
-public record Score(@Id Long id, Long points, String username, String mode) {
+public record Score(
+        @Schema(readOnly = true)
+        @Id Long id,
+        Long points,
+        String username,
+        String mode
+) {
 }
